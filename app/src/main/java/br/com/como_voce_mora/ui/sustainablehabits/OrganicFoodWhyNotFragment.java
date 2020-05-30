b6781a3a3a0dcbf66231633ecac58773804d3e6f
Matheus Souza <matheus.alex.souza@gmail.com>
2020-05-30 18:04:14 -0300

    More changes

--- app/src/main/java/br/com/como_voce_mora/ui/sustainablehabits/OrganicFoodWhyNotFragment.java
+++ app/src/main/java/br/com/como_voce_mora/ui/sustainablehabits/OrganicFoodWhyNotFragment.java
@@ -62,7 +62,6 @@ public class OrganicFoodWhyNotFragment extends BaseFragment implements CustomRad
 
     @Override
     public void init() {
-        super.init();
         mProgress.setProgress(HowYouLiveProgressBar.HowYouLive.HABITS);
         rbBrushMyTeeth.setOnCheckedChangeListener(this);
         rbDishes.setOnCheckedChangeListener(this);
